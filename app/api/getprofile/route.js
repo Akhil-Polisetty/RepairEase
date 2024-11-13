054beb0e755a0c5e12989e6cd4ce6e2100dafd51
Akhil-Polisetty <polisettiakhi72073@gmail.com>
2024-11-13 15:22:05 +0530

    added changes in get profile

--- app/api/getprofile/route.js
+++ app/api/getprofile/route.js
@@ -1,24 +1,24 @@
-app.post('/getprofile', (req, res) => {
-    // const email_cookie = Cookies.get("email");
-    // if (!email_cookie) {
-    //   console.log("cookie not found");
-    //   return res.status(400).json("Email cookie not found");
-    // }
-      // const email_cookie = Cookies.get('email')
-      // console.log("the cookie is ",email_cookie)
-      // console.log("email at getusering is ",email_glob)
-      const {email_id}=req.body;
-      console.log("entered server usering")
-      console.log("email is ",email_id);
+// app.post('/getprofile', (req, res) => {
+//     // const email_cookie = Cookies.get("email");
+//     // if (!email_cookie) {
+//     //   console.log("cookie not found");
+//     //   return res.status(400).json("Email cookie not found");
+//     // }
+//       // const email_cookie = Cookies.get('email')
+//       // console.log("the cookie is ",email_cookie)
+//       // console.log("email at getusering is ",email_glob)
+//       const {email_id}=req.body;
+//       console.log("entered server usering")
+//       console.log("email is ",email_id);
   
-      RuserModel.findOne({ email_id:email_id})
-      .then(usering => {
-        if (usering) {
-          console.log(usering)
-          res.json(usering);
-        } else {
-          res.status(404).json("User not found");
-        }
-      })
-      .catch(err => res.status(500).json({ error: err.message }));
-  });
+//       RuserModel.findOne({ email_id:email_id})
+//       .then(usering => {
+//         if (usering) {
+//           console.log(usering)
+//           res.json(usering);
+//         } else {
+//           res.status(404).json("User not found");
+//         }
+//       })
+//       .catch(err => res.status(500).json({ error: err.message }));
+//   });
