02ea866b8c5af7cb9be0d66f0c2e0dfecce05a29
Akhil-Polisetty <polisettiakhi72073@gmail.com>
2024-11-13 15:13:17 +0530

    added changes in auth

--- app/api/auth/logout/route.js
+++ app/api/auth/logout/route.js
@@ -1,6 +1,6 @@
-app.get('/auth/logout',(req,res)=>
-    {
-      res.clearCookie('token')
-      res.clearCookie('email')
-      return res.json({status:true})
-    })
+// app.get('/auth/logout',(req,res)=>
+//     {
+//       res.clearCookie('token')
+//       res.clearCookie('email')
+//       return res.json({status:true})
+//     })
