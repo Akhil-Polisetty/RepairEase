10cb9e0deb152988bf1a4d21a9be9607b6453cc3
Akhil-Polisetty <polisettiakhi72073@gmail.com>
2024-11-13 15:19:11 +0530

    commented getactive-rep

--- app/api/getactive_rep/route.js
+++ app/api/getactive_rep/route.js
@@ -1,18 +1,18 @@
-app.get('/getactiverep',(req,res)=>{
-    ActiveRepairModel.find({
-      $or: [
-        { user: email_glob },
-        { technician: email_glob }
-      ]
-    }).then(actives => {
-      if(actives){
-        console.log("responses are ",actives)
-        res.json(actives);
-      }
-      else
-      {
-        res.status(404).json("responses not found");
-      }
-    })
-    .catch(err => res.status(500).json({error:err.message}));
-  });
+// app.get('/getactiverep',(req,res)=>{
+//     ActiveRepairModel.find({
+//       $or: [
+//         { user: email_glob },
+//         { technician: email_glob }
+//       ]
+//     }).then(actives => {
+//       if(actives){
+//         console.log("responses are ",actives)
+//         res.json(actives);
+//       }
+//       else
+//       {
+//         res.status(404).json("responses not found");
+//       }
+//     })
+//     .catch(err => res.status(500).json({error:err.message}));
+//   });
